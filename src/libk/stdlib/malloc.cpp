081cbec150154f1fb0077371a9dbf1e2eff0059b
Ruan <ruan@pysoft.co.za>
2025-09-24 22:54:02 +0200

    Patch malloc for better realloc
    Basically, prefer to initially allocate objects far apart in memory
    rather than right after one another resulting in large gaps of free
    memory between objects, which can be used by calls to realloc to avoid
    the expense of malloc + memcpy + free.
    
    This could be improved by adding multiple runs, so that instead of
    allocation looking like so:
     1 5 9 * 2 6 * * 3 7 * * 4 8 * *
    It instead looks like so:
     1 9 5 * 2 * 6 * 3 * 7 * 4 * 8 *
    However, I can't be bothered to figure the algorithm for that out at the
    moment, what I have is already much better than what I initially had.

--- src/libk/stdlib/malloc.cpp
+++ src/libk/stdlib/malloc.cpp
@@ -14,23 +14,30 @@ void *malloc(size_t size) {
 		++blocks_to_alloc;
 	}
 
-	for (size_t i = 0; i < NUM_BLOCKS; ++i) {
-		alloc_header_t *header;
+	// very simple adjustment to make the memory allocator slightly more
+	// likely to result in efficient reallocs
+	constexpr size_t DESIRED_SPACING = NUM_BLOCKS / 512;
+	for (size_t small_step = 0; small_step < DESIRED_SPACING; ++small_step) {
+		for (size_t large_step = 0; large_step < NUM_BLOCKS; large_step += DESIRED_SPACING) {
+			const size_t i = large_step + small_step;
 
-		for (size_t j = 0; j < blocks_to_alloc; ++j) {
-			if (get_used(i+j)) goto no_alloc;
+			alloc_header_t *header;
+
+			for (size_t j = 0; j < blocks_to_alloc; ++j) {
+				if (get_used(i+j)) goto no_alloc;
+			}
+
+			for (size_t j = 0; j < blocks_to_alloc; ++j) {
+				set_used(i+j);
+			}
+
+			header = (alloc_header_t*)get_ptr(i);
+			header->s.num_blocks = blocks_to_alloc;
+
+			return header+1;
+
+		no_alloc:;
 		}
-
-		for (size_t j = 0; j < blocks_to_alloc; ++j) {
-			set_used(i+j);
-		}
-
-		header = (alloc_header_t*)get_ptr(i);
-		header->s.num_blocks = blocks_to_alloc;
-
-		return header+1;
-
-	no_alloc:;
 	}
 
 	// didn't find a large enough contiguous block of free memory
