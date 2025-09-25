faad216f29eef34920fd7ff05b3799b62bba3174
Ruan <ruan@pysoft.co.za>
2025-09-25 08:51:48 +0200

    Set up and load Global Descriptor Table
    Decided to go with a flat model, where the kernel has both code and data
    access to the entire memory space. There is no user mode segments, as I
    do not plan on supporting a user mode, and there is no task state
    segment, since as I understand it that is only needed for multitasking.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -19,21 +19,65 @@
 #error "This tutorial needs to be compiled with an ix86-elf compiler"
 #endif
 
-extern "C" {
+static inline uint64_t create_gdt_entry(uint32_t base, uint32_t limit, uint8_t flags, uint8_t access) {
+	// from https://wiki.osdev.org/GDT_Tutorial
+	// and https://wiki.osdev.org/Global_Descriptor_Table
+	uint64_t res = 0;
+
+	// first, set up the high 32 bits:
+
+	res |= base  & 0xFF000000;        // base 24:31
+	res |= uint32_t(flags&0xF) << 20; // flags 0:3
+	res |= limit & 0x000F0000;        // limit 16:19
+	res |= uint32_t(access) << 8;     // access 0:7
+	res |= (base >> 16) & 0xFF;       // base 16:23
+
+	res <<= 32;
+
+	// next, set up the low 32 bits:
+
+	res |= base << 16;         // base 0:15
+	res |= limit & 0x0000FFFF; // limit 0:15
+
+	return res;
+}
+
 /*
  * make the kernel_main function extern "C"
  * bc idk what funky stuff C++ does with calling conventions,
  * this way I know how to call it from assembly
  */
 /* also, to avoid name mangling */
+extern "C" void kernel_early_main(void);
+extern "C" void kernel_main(void);
 
-void kernel_early_main(void) {
+static uint64_t gdt[3];
+static uint64_t gdt_descriptor = 0;
+void kernel_early_main() {
 	/* Initialize terminal interface */
 	term_init();
-}
 
-void kernel_main(void);
+	// set up the GDT
+	// see https://wiki.osdev.org/GDT_Tutorial
+	// and https://wiki.osdev.org/Global_Descriptor_Table
 
+	// flat setup, only kernel (ring 0)
+	const uint64_t gdt_null = create_gdt_entry(0, 0, 0, 0);
+	const uint64_t gdt_kernel_code = create_gdt_entry(0, 0xFFFFF, 0xC, 0x9A);
+	const uint64_t gdt_kernel_data = create_gdt_entry(0, 0xFFFFF, 0xC, 0x92);
+
+	gdt[0] = gdt_null;
+	gdt[1] = gdt_kernel_code;
+	gdt[2] = gdt_kernel_data;
+
+	const uint16_t gdt_size = sizeof(gdt)-1;
+	const uint32_t gdt_addr = uint32_t(gdt);
+
+	gdt_descriptor = gdt_addr;
+	gdt_descriptor <<= 16;
+	gdt_descriptor |= gdt_size;
+
+	asm volatile("lgdt %[gdtr]" :: [gdtr] "m" (gdt_descriptor) : "memory");
 }
 
 class Foo {
