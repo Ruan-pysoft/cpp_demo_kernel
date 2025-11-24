6460bd83208da3d95ac9450311fb539aba90cb55
Ruan <ruan@pysoft.co.za>
2025-11-24 21:23:59 +0200

    Snake: Implement game over and better movement

--- src/apps/snake.cpp
+++ src/apps/snake.cpp
@@ -1,6 +1,7 @@
 #include "apps/snake.hpp"
 
 #include <stdint.h>
+#include <string.h>
 
 #include "eventloop.hpp"
 #include "pit.hpp"
@@ -41,6 +42,11 @@ public:
 
 struct Pos {
 	uint8_t x, y;
+
+	bool operator==(const struct Pos &other) const {
+		return x == other.x && y == other.y;
+	}
+
 	static Pos random_pos(Prng &prng) {
 		const uint32_t rand = prng.next();
 		// technically biased towards lower numbers, but who cares?
@@ -51,13 +57,16 @@ struct Pos {
 	}
 };
 
+struct State;
 constexpr size_t MAX_SNAKE_SIZE = VGA_WIDTH*VGA_HEIGHT;
 class Snake {
 	Pos segments[MAX_SNAKE_SIZE];
 	size_t num_segments;
-public:
-	Direction facing;
 
+	Direction prev_facing;
+	bool has_prev_facing = false;
+	Direction facing;
+public:
 	Snake(Pos start = {VGA_WIDTH/2, VGA_HEIGHT/2}, size_t initial_segments = 5, Direction initial_dir = Direction::Left)
 	: num_segments(initial_segments), facing(initial_dir) {
 		for (size_t i = 0; i < num_segments; ++i) {
@@ -65,31 +74,20 @@ public:
 		}
 	}
 
-	void move() {
-		size_t i = num_segments;
-		while (i --> 1) {
-			segments[i] = segments[i-1];
+	Direction get_facing() const {
+		return facing;
+	}
+	void look(Direction dir) {
+		if (has_prev_facing) {
+			if (prev_facing == Direction::Left && dir == Direction::Right) return;
+			if (prev_facing == Direction::Down && dir == Direction::Up) return;
+			if (prev_facing == Direction::Up && dir == Direction::Down) return;
+			if (prev_facing == Direction::Right && dir == Direction::Left) return;
 		}
-		switch (facing) {
-			case Direction::Left: {
-				if (segments[0].x == 0) segments[0].x = VGA_WIDTH - 1;
-				else --segments[0].x;
-			} break;
-			case Direction::Down: {
-				if (segments[0].y == VGA_HEIGHT - 1) segments[0].y = 0;
-				else ++segments[0].y;
-			} break;
-			case Direction::Up: {
-				if (segments[0].y == 0) segments[0].y = VGA_HEIGHT - 1;
-				else --segments[0].y;
-			} break;
-			case Direction::Right: {
-				if (segments[0].x == VGA_WIDTH - 1) segments[0].x = 0;
-				else ++segments[0].x;
-			} break;
-		}
+		facing = dir;
 	}
 
+	void update(State &state);
 	void draw() const {
 		size_t i = num_segments;
 		while (i --> 0) {
@@ -101,6 +99,8 @@ public:
 
 struct State {
 	bool should_quit = false;
+	bool lost = false;
+	bool blink_state = false;
 	Prng prng{}; // I assume the constructor gets called each tame State is created? idk, I'll figure it out later
 	Snake snake{};
 	Pos apple;
@@ -118,22 +118,22 @@ void handle_keypress(State *state, ps2::Event event) {
 			case ps2::KEY_LEFT:
 			case ps2::KEY_A:
 			case ps2::KEY_H: {
-				state->snake.facing = Direction::Left;
+				state->snake.look(Direction::Left);
 			} break;
 			case ps2::KEY_DOWN:
 			case ps2::KEY_D:
 			case ps2::KEY_J: {
-				state->snake.facing = Direction::Down;
+				state->snake.look(Direction::Down);
 			} break;
 			case ps2::KEY_UP:
 			case ps2::KEY_W:
 			case ps2::KEY_K: {
-				state->snake.facing = Direction::Up;
+				state->snake.look(Direction::Up);
 			} break;
 			case ps2::KEY_RIGHT:
 			case ps2::KEY_S:
 			case ps2::KEY_L: {
-				state->snake.facing = Direction::Right;
+				state->snake.look(Direction::Right);
 			} break;
 			default: break;
 		}
@@ -148,10 +148,77 @@ void draw(State &state) {
 	term_putchar('a');
 
 	state.snake.draw();
+
+	if (state.lost) {
+		const char *lose_text =  " ** GAME OVER ** ";
+		const char *frame_text = "                 ";
+		const size_t lose_text_len = strlen(lose_text);
+		const size_t lose_text_offset = (VGA_WIDTH - lose_text_len)/2;
+
+		if (state.blink_state) {
+			term_setcolor(vga_entry_color(
+				vga_color::white,
+				vga_color::red
+			));
+		} else {
+			term_setcolor(vga_entry_color(
+				vga_color::red,
+				vga_color::white
+			));
+		}
+
+		term_goto(lose_text_offset, VGA_HEIGHT/2 - 1);
+		term_writestring(frame_text);
+		term_goto(lose_text_offset, VGA_HEIGHT/2);
+		term_writestring(lose_text);
+		term_goto(lose_text_offset, VGA_HEIGHT/2 + 1);
+		term_writestring(frame_text);
+
+		term_resetcolor();
+	}
 }
 
 void update(State &state) {
-	state.snake.move();
+	if (!state.lost) state.snake.update(state);
+	state.blink_state = !state.blink_state;
+}
+
+void Snake::update(State &state) {
+	has_prev_facing = true;
+	prev_facing = facing;
+
+	Pos next_head_pos = segments[0];
+	switch (facing) {
+		case Direction::Left: {
+			next_head_pos.x = next_head_pos.x == 0
+				? VGA_WIDTH - 1 : next_head_pos.x - 1;
+		} break;
+		case Direction::Down: {
+			next_head_pos.y = next_head_pos.y == VGA_HEIGHT - 1
+				? 0 : next_head_pos.y + 1;
+		} break;
+		case Direction::Up: {
+			next_head_pos.y = next_head_pos.y == 0
+				? VGA_HEIGHT - 1 : next_head_pos.y - 1;
+		} break;
+		case Direction::Right: {
+			next_head_pos.x = next_head_pos.x == VGA_WIDTH - 1
+				? 0 : next_head_pos.x + 1;
+		} break;
+	}
+
+	for (size_t i = 1; i < num_segments; ++i) {
+		if (next_head_pos == segments[i]) {
+			state.lost = true;
+			return;
+		}
+	}
+
+	size_t i = num_segments;
+	while (i --> 1) {
+		segments[i] = segments[i-1];
+	}
+	segments[0] = next_head_pos;
 }
 
 }
