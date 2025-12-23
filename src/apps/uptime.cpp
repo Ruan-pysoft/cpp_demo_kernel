40db37097fff6b330b83b5abaee74c8d503284f7
Ruan <ruan@pysoft.co.za>
2025-12-23 23:17:54 +0200

    Minor cosmetic upgrade to uptime tracker

--- src/apps/uptime.cpp
+++ src/apps/uptime.cpp
@@ -16,22 +16,47 @@ void draw() {
 
 	const uint32_t now = pit::millis;
 
+	uint32_t millis = now;
 	uint32_t seconds = now / 1000;
+	millis -= seconds * 1000;
 	uint32_t minutes = seconds / 60;
 	seconds -= minutes * 60;
 	uint32_t hours = minutes / 60;
 	minutes -= hours * 60;
+	uint32_t days = hours / 24;
+	hours -= days * 24;
 
 	term::clear();
 	term::go_to(0, 0);
 
-	printf("The system has been online for %u hours, %u minutes, and %u seconds.\n", hours, minutes, seconds);
+	char time_display[] = "HH:MM:SS.MMM";
+
+	time_display[0] = '0' + hours/10;
+	time_display[1] = '0' + hours%10;
+
+	time_display[3] = '0' + minutes/10;
+	time_display[4] = '0' + minutes%10;
+
+	time_display[6] = '0' + seconds/10;
+	time_display[7] = '0' + seconds%10;
+
+	time_display[9] = '0' + millis/100;
+	time_display[10] = '0' + (millis/10)%10;
+	time_display[11] = '0' + millis%10;
+
+	if (days) {
+		printf("System Uptime: %u days, %s\n", days, time_display);
+	} else {
+		printf("System Uptime: %s\n", time_display);
+	}
 	puts("Press Q or ESC to quit.");
 }
 
 }
 
 void main() {
+	// TEST: pit::millis += 542867211;
+
 	for (;;) {
 		draw();
 
