dc0674ebd744b4b7e31f2a1952454de124837789
Ruan <ruan@pysoft.co.za>
2025-12-24 22:32:45 +0200

    Add string type & upgrade list type (& fix semantics?)

--- include/libk/sdk/util.hpp
+++ include/libk/sdk/util.hpp
@@ -1,12 +1,29 @@
 #pragma once
 
 #include <assert.h>
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <cppsupport.hpp>
 
 namespace sdk::util {
+
+template<typename T>
+struct RemoveReference { using type = T; };
+template<typename T>
+struct RemoveReference<T&> { using type = T; };
+template<typename T>
+struct RemoveReference<T&&> { using type = T; };
+
+// written using ChatGPT bc I have no idea what I'm doing here
+// actually, I have a copy of "The C++ Programming Language",
+// I wonder if I could figure this out...
+template<typename T>
+typename RemoveReference<T>::type&& move(T&& arg) noexcept {
+	return static_cast<typename RemoveReference<T>::type&&>(arg);
+}
 
 template<typename T>
 struct Maybe {
@@ -25,7 +42,7 @@ struct Maybe {
 
 	inline const T &get() const {
 		assert(has);
-		return *(T*)storage;
+		return *(const T*)storage;
 	}
 	inline T &get() {
 		assert(has);
@@ -54,25 +71,82 @@ struct Pair {
 
 template<typename T>
 class List {
-	T *buf;
+	using memory_cell_t = uint8_t[sizeof(T)];
+	alignas(T*) memory_cell_t *buf;
 	size_t len;
 	size_t cap;
+
+	void grow_cap() {
+		cap *= 2;
+		if (cap == 0) cap = 64;
+		buf = (memory_cell_t*)realloc(buf, cap);
+		assert(buf != NULL);
+	}
 public:
 	using iterator = T*;
+	using const_iterator = const T*;
 
 	List(size_t capacity = 64)
-	: buf((T*)calloc(capacity, sizeof(T))), len(0), cap(capacity) {
+	: len(0), cap(capacity) {
+		buf = (memory_cell_t*)calloc(capacity, sizeof(T));
 		assert(buf != NULL);
 		assert(cap != 0);
+	}
+	~List() {
+		if (buf) {
+			for (auto &elem : *this) {
+				elem.~T();
+			}
+			free(buf);
+		}
+	}
+
+	List(const List &other) : List(other.cap) {
+		for (const auto &elem : other) {
+			this->push_back(elem);
+		}
+	}
+	List &operator=(const List &other) {
+		for (auto &elem : *this) {
+			elem.~T();
+		}
+		this->len = 0;
+		for (const auto &elem : other) {
+			this->push_back(elem);
+		}
+	}
+	List(List &&other) {
+		this->buf = other.buf;
+		this->len = other.len;
+		this->cap = other.cap;
+		other.buf = nullptr;
+		other.len = 0;
+		other.cap = 0;
+	}
+	List &operator=(List &&other) {
+		if (this->buf) {
+			for (auto &elem : *this) {
+				elem.~T();
+			}
+			free(this->buf);
+		}
+		this->buf = other.buf;
+		this->len = other.len;
+		this->cap = other.cap;
+		other.buf = nullptr;
+		other.len = 0;
+		other.cap = 0;
+
+		return *this;
 	}
 
 	const T &operator[](size_t index) const {
 		assert(index < len);
-		return buf[index];
+		return *(const T*)&buf[index];
 	}
 	T &operator[](size_t index) {
 		assert(index < len);
-		return buf[index];
+		return *(T*)&buf[index];
 	}
 
 	size_t size() const { return len; }
@@ -80,30 +154,121 @@ public:
 
 	void push_back(const T &val) {
 		if (len == cap) {
-			cap *= 2;
-			buf = (T*)reallocarray(buf, cap, sizeof(T));
-			assert(buf != NULL);
+			grow_cap();
 		}
-		buf[len++] = val;
+		new ((void*)&buf[len++]) T(val);
 	}
 	void push_back(T &&val) {
 		if (len == cap) {
-			cap *= 2;
-			buf = (T*)reallocarray(buf, cap, sizeof(T));
-			assert(buf != NULL);
+			grow_cap();
 		}
-		buf[len++] = val;
+		new ((void*)&buf[len++]) T(val);
 	}
-	T pop_back() {
-		return buf[--len];
+	T &&pop_back() {
+		return move(*(T*)&buf[--len]);
 	}
 
-	T *begin() {
-		return &buf[0];
+	iterator begin() { return (iterator)&buf[0]; }
+	const_iterator begin() const { return (const_iterator)&buf[0]; }
+	iterator end() { return (iterator)&buf[len]; }
+	const_iterator end() const { return (const_iterator)&buf[len]; }
+
+	// TODO:
+	/*
+	void erase(size_t pos, size_t len) {
+		assert(pos <= len && pos + len <= len);
+		...
 	}
-	T *end() {
-		return &buf[len];
+	*/
+
+	T &back() {
+		assert(len > 0);
+		return *(T*)&buf[len-1];
 	}
+	const T &back() const {
+		assert(len > 0);
+		return *(const T*)&buf[len-1];
+	}
+};
+
+class String {
+	char *buf;
+	size_t len;
+	size_t cap;
+
+	void grow_cap();
+	void add_char_unsafe(char c);
+	void add_null_terminator();
+public:
+	using iterator = char *;
+	using const_iterator = const char *;
+
+	String(size_t capacity = 64)
+	: len(0), cap(capacity) {
+		buf = (char*)calloc(capacity, sizeof(char));
+		assert(buf != NULL);
+		assert(cap != 0);
+	}
+	String(const char *str) : String(str, strlen(str)) { }
+	String(const char *str, size_t len) : String(len+1) {
+		// no need for explicit null terminator,
+		// buffer is calloc'd so zeroed by default
+		for (size_t i = 0; i < len; ++i) {
+			buf[i] = str[i];
+		}
+	}
+	~String() {
+		if (buf) free(buf);
+	}
+
+	String(const String &str) : String(str.buf, str.len) { }
+	String &operator=(const String &str) {
+		this->len = 0;
+		return *this += str;
+	}
+	String(String &&str) {
+		this->buf = str.buf;
+		this->len = str.len;
+		this->cap = str.cap;
+		str.buf = nullptr;
+		str.len = 0;
+		str.cap = 0;
+	}
+	String &operator=(String &&str) {
+		if (this->buf) free(this->buf);
+		this->buf = str.buf;
+		this->len = str.len;
+		this->cap = str.cap;
+		str.buf = nullptr;
+		str.len = 0;
+		str.cap = 0;
+
+		return *this;
+	}
+
+	const char &operator[](size_t index) const;
+	char &operator[](size_t index);
+
+	size_t size() const { return len; }
+	size_t capacity() const { return cap; }
+
+	void erase(size_t pos, size_t len);
+	void erase(size_t from);
+	void erase(const_iterator a, const_iterator b);
+	void erase(const_iterator a);
+
+	iterator begin() { return &buf[0]; }
+	const_iterator begin() const { return &buf[0]; }
+	iterator end() { return &buf[len]; }
+	const_iterator end() const { return &buf[len]; }
+
+	const char *c_str() const { return buf; }
+
+	String &operator+=(char c);
+	String &operator+=(const char *str);
+	String &operator+=(const String &other);
+
+	void write() const;
 };
 
 }
--- src/libk/sdk/util.cpp
+++ src/libk/sdk/util.cpp
@@ -1,1 +1,86 @@
 #include <sdk/util.hpp>
+
+#include "vga.hpp"
+
+namespace sdk::util {
+
+void String::grow_cap() {
+	cap *= 2;
+	if (cap == 0) cap = 64;
+	buf = (char*)realloc(buf, cap);
+	assert(buf != NULL);
+}
+void String::add_char_unsafe(char c) {
+	if (len == cap) {
+		grow_cap();
+	}
+
+	buf[len++] = c;
+}
+void String::add_null_terminator() {
+	if (len+1 == cap) {
+		grow_cap();
+	}
+
+	buf[len] = 0;
+}
+
+const char &String::operator[](size_t index) const {
+	assert(index < len);
+	return buf[index];
+}
+char &String::operator[](size_t index) {
+	assert(index < len);
+	return buf[index];
+}
+
+void String::erase(size_t pos, size_t len) {
+	assert(pos <= this->len && pos + len <= this->len);
+
+	if (len == 0) return;
+
+	for (size_t i = 0; pos + len + i < this->len; ++i) {
+		buf[pos + i] = buf[pos + len + i];
+	}
+	this->len -= len;
+	add_null_terminator();
+}
+void String::erase(size_t from) {
+	this->erase(from, len - from);
+}
+void String::erase(String::const_iterator a, String::const_iterator b) {
+	assert(a <= b);
+	assert(this->begin() <= a);
+	assert(b <= this->end());
+
+	this->erase(a - this->begin(), b - a);
+}
+void String::erase(String::const_iterator a) {
+	this->erase(a, this->end());
+}
+
+String &String::operator+=(char c) {
+	add_char_unsafe(c);
+	add_null_terminator();
+	return *this;
+}
+String &String::operator+=(const char *str) {
+	for (size_t i = 0; str[i] != 0; ++i) {
+		add_char_unsafe(str[i]);
+	}
+	add_null_terminator();
+	return *this;
+}
+String &String::operator+=(const String &other) {
+	for (const auto ch : other) {
+		add_char_unsafe(ch);
+	}
+	add_null_terminator();
+	return *this;
+}
+
+void String::write() const {
+	term::write(buf, len);
+}
+
+}
