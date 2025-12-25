186e3c56bccee2cf279b552ef33b40d8398dff5b
Ruan <ruan@pysoft.co.za>
2025-12-25 10:37:35 +0200

    Editor: redo drawing logic & draw line numbers

--- src/apps/editor.cpp
+++ src/apps/editor.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #include <sdk/util.hpp>
+#include <sdk/terminal.hpp>
 
 #include "ps2.hpp"
 #include "vga.hpp"
@@ -19,30 +20,111 @@ struct State {
 };
 
 struct File {
-	struct Cursor {
+	struct Pos {
 		size_t line = 0;
 		size_t col = 0;
-	} cursor;
+	};
+	Pos cursor;
+	Pos screen_top;
 	List<String> lines {};
+
+	size_t line_num_width() {
+		size_t width = 1;
+		size_t lines = this->lines.size();
+		while (lines) {
+			lines /= 10;
+			width += 1;
+		}
+		return width;
+	}
+	size_t line_content_width() {
+		return vga::WIDTH - line_num_width();
+	}
+	size_t visual_lines(size_t line_idx) {
+		assert(line_idx < lines.size());
+		const auto &line = lines[line_idx];
+		if (line.size() == 0) return 1;
+		const auto content_width = line_content_width();
+		return line.size()/content_width + !!(line.size()%content_width);
+	}
+
+	void advance(Pos &pos) {
+		if (pos.line >= lines.size()) {
+			pos.line = lines.size();
+			pos.col = 0;
+			return;
+		}
+		if (pos.col >= lines.size()) {
+			++pos.line;
+			pos.col = 0;
+			return;
+		}
+
+		const size_t rest = lines[pos.line].size() - pos.col;
+		const size_t content_width = line_content_width();
+
+		if (rest > content_width) {
+			pos.col += content_width;
+		} else {
+			++pos.line;
+			pos.col = 0;
+		}
+	}
+	void write_from(size_t row, Pos pos) {
+		assert(pos.line < lines.size());
+		assert(pos.col < lines[pos.line].size() || pos.col == 0);
+
+		const char *const data = lines[pos.line].c_str() + pos.col;
+		const size_t data_len = lines[pos.line].size() - pos.col;
+		const size_t content_width = line_content_width();
+
+		term::go_to(line_num_width(), row);
+		if (data_len == 0) {
+			assert(pos.col == 0);
+		} else if (data_len > content_width) {
+			term::write(data, content_width);
+		} else {
+			term::writestring(data);
+		}
+	}
+	void write_lineno(size_t row, size_t lineno) {
+		const auto _ = sdk::ColorSwitch(vga::Color::Black, vga::Color::LightGrey);
+
+		size_t col = line_num_width() - 2;
+
+		do {
+			term::go_to(col, row);
+			term::putchar('0' + lineno%10);
+			lineno /= 10;
+			if (col > 0) --col;
+			else return;
+		} while (lineno);
+		term::go_to(0, row);
+		for (size_t c = 0; c <= col; ++c) {
+			term::putchar(' ');
+		}
+	}
+	void draw() {
+		const auto _ = term::Backbuffer();
+
+		term::clear();
+		term::go_to(0, 0);
+
+		Pos draw_from = screen_top;
+		for (size_t i = 0; i < vga::HEIGHT; ++i) {
+			if (draw_from.line >= lines.size()) break;
+			if (draw_from.col == 0) {
+				write_lineno(i, draw_from.line+1);
+			}
+			write_from(i, draw_from);
+			advance(draw_from);
+		}
+	}
 };
 
 struct File file {};
 
 State state;
-
-void display_buffer() {
-	const auto _ = term::Backbuffer();
-
-	term::clear();
-	term::go_to(0, 0);
-
-	for (const String *line = file.lines.begin(); line != file.lines.end(); ++line) {
-		if (line != file.lines.begin()) {
-			term::putchar('\n');
-		}
-		line->write();
-	}
-}
 
 void input_key(ps2::Key key, char ch, bool capitalise) {
 	if (key == ps2::KEY_BACKSPACE) {
@@ -53,6 +135,7 @@ void input_key(ps2::Key key, char ch, bool capitalise) {
 		}
 		return;
 	} else if (key == ps2::KEY_ENTER) {
+		if (file.lines.size() == 0) file.lines.push_back({});
 		file.lines.push_back({});
 	} else {
 		if (capitalise && 'a' <= ch && ch <= 'z') ch ^= 'a'^'A';
@@ -89,7 +172,9 @@ void handle_keyevent(ps2::EventType type, ps2::Key key) {
 void main() {
 	state = State{};
 
-	display_buffer();
+	term::disable_autoscroll();
+
+	file.draw();
 
 	while (!state.should_quit) {
 		const bool had_events = !ps2::events.empty();
@@ -99,7 +184,7 @@ void main() {
 			handle_keyevent(event.type, event.key);
 		}
 
-		if (had_events) display_buffer();
+		if (had_events) file.draw();
 
 		__asm__ volatile("hlt" ::: "memory");
 	}
